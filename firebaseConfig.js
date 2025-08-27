11dc9a11e209eccf5beb644a364527b4995dfc09
valjosephmantuhac-collab <valjosephmantuhac@gmail.com>
2025-08-27 09:48:41 +0800

    Firebase Config

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDvJc62mx2WJ_zUwM9uTwIl95B4ezUB0Hw",
+  authDomain: "lapisgoals.firebaseapp.com",
+  projectId: "lapisgoals",
+  storageBucket: "lapisgoals.firebasestorage.app",
+  messagingSenderId: "125033866655",
+  appId: "1:125033866655:web:65cb5601de788255bfc9aa",
+  measurementId: "G-4LZ3NXJF4V"
+};
+
+const app = initializeApp(firebaseConfig);
