2611d8f35be215ccc8ccbe98f47bab87c5f11793
ponce-goals <valjosephmantuhac@gmail.com>
2025-09-03 09:28:58 +0800

    Create Goals

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 
 const firebaseConfig = {
@@ -12,3 +13,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
